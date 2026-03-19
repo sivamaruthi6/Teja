3a50cfa532c9630816488ec8b25de6ae4b582d2d
sivamaruthi6 <sivamaruthisiva@gmail.com>
2026-03-19 16:35:09 +0530

    first commit

---
+++ employee.js
@@ -0,0 +1,18 @@
+const employees = [
+  {
+    empnumber: 100,
+    empname: "A",
+    empsalary: 10000
+  },
+  {
+    empnumber: 200,
+    empname: "B",
+    empsalary: 20000
+  }
+];
+
+module.exports = employees;
+
+
+just for TextDecoderStream.
+
